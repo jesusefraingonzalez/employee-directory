8f58b4a3e354383d76bab89e938097960c21a45f
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-07 14:01:35 -0500

    add user row component

---
+++ src/components/UserRow.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserRow extends Component {
+    
+    render() {
+        return (
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+        )
+    }
+
+}
