fd11ca70726f5ad477cf6826e44a9bc48ec24b7d
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-07 14:59:06 -0500

    add logic for numUsers

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 
 export default {
     getRandomUsers: (numUsers) => {
-        let url = `https://randomuser.me/api/?results=${numUsers}&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
-        
+        let url;
+        if (numUsers) url = `https://randomuser.me/api/?results=${numUsers}&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
+        else url = `https://randomuser.me/api/?results=100&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
+
         return axios.get(url)
             .then(({ data }) => {
                 console.log(data);
