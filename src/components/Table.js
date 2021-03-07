216a6c61d34d8a9019789c0bee9de877fea7e6fc
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-06 23:44:05 -0500

    add populate user function using random user api

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const numUsers = 100;
 const url = `https://randomuser.me/api/?results=${numUsers}`;
 
 const Table = () => {
-const [user, setUser] = useState();
+    const [user, setUser] = useState();
 
-
+    const populateUsers = () => {
+        axios.get(url)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.error(err));
+    }
 }
