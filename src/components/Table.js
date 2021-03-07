b6613bd41cfbb99d1f3a53231b13c93817213357
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-07 15:21:20 -0500

    get users on table mount

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import '../utils/API';
+import API from '../utils/API';
 
-class Table extends Component {   
+class Table extends Component {
+    state = {
+        employees =[],
+        sorted =[]
+    }
+
+    // make api call when the table mounts
+    componentDidMount() {
+        API.getRandomUsers(2)
+            .then(({ data }) => {
+                console.log(data.results);
+                this.setState({
+                    employees: [...data.results]
+                })
+            }).catch(err => console.error(err));
+    }
+
     render() {
         return (
             <div>
                 <table>
                     <tr>
                         <th></th>
+                        test
                         <th></th>
                     </tr>
                 </table>
@@ -15,3 +32,5 @@ class Table extends Component {
         )
     }
 }
+
+export default Table;
