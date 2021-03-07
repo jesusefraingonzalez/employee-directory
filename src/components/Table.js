9c2afe0bc8a7b16ef038ca3d69fb650c72833c49
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-07 14:36:13 -0500

    remove populate function

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { Component } from 'react';
+import '../utils/API';
 
-const numUsers = 100;
-const url = `https://randomuser.me/api/?results=${numUsers}&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
-
-const Table = () => {
-    const [users, setUsers] = useState();
-
-    const populateUsers = () => {
-        axios.get(url)
-            .then(({ data }) => {
-                console.log(data);
-                setUsers(data.results);
-            })
-            .catch(err => console.error(err));
+class Table extends Component {   
+    render() {
+        return (
+            <div>
+                <table>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </table>
+            </div>
+        )
     }
-
-    return (
-        <div>
-            <table>
-                <tr>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </table>
-        </div>
-    )
 }
