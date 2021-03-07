2c743d6fbb6e443fab4ec1a7f22a81ca420bb166
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-06 23:42:27 -0500

    begin Table component

---
+++ src/components/Table.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const numUsers = 100;
+const url = `https://randomuser.me/api/?results=${numUsers}`;
+
+const Table = () => {
+const [user, setUser] = useState();
+
+
+}
