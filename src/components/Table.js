101f6e534c902ab4fafadd30e8e7f551e9e8f157
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-08 02:45:18 -0500

    render UserRow elements in table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import API from '../utils/API';
+import UserRow from './UserRow';
 
 class Table extends Component {
     state = {
-        employees =[],
-        sorted =[]
+        employees: [],
+        sorted: []
     }
 
     // make api call when the table mounts
     componentDidMount() {
         API.getRandomUsers(2)
-            .then(({ data }) => {
-                console.log(data.results);
-                this.setState({
-                    employees: [...data.results]
-                })
-            }).catch(err => console.error(err));
+        .then(({ data }) => {
+            // console.log(data.results);
+            this.setState({
+                employees: [...data.results]
+            });
+        }).catch(err => console.error(err));
     }
 
     render() {
+  
         return (
             <div>
                 <table>
-                    <tr>
-                        <th></th>
-                        test
-                        <th></th>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Phone</th>
+                            <th>Cell</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.employees.map(employee => {
+                            return(
+                                <UserRow employee={employee}/>
+                            );
+                        })}
+                    </tbody>
+
                 </table>
             </div>
         )
