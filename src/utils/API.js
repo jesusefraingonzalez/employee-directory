89f04ca9614dba4ab329cea10d95b8d91b3d8633
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-08 00:00:02 -0500

    restructure api.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
-
-export default {
+const API = {
     getRandomUsers: (numUsers) => {
         let url;
         if (numUsers) url = `https://randomuser.me/api/?results=${numUsers}&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
         else url = `https://randomuser.me/api/?results=100&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
 
-        return axios.get(url)
-            .then(({ data }) => {
-                console.log(data);
-            })
-            .catch(err => console.error(err));
+        return axios.get(url);
     }
 }
+
+export default API;
