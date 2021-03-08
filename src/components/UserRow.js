bec8f7ae205a4fbdadbc5040eb6cb1bea5860652
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-08 00:43:17 -0500

    destructure the props

--- src/components/UserRow.js
+++ src/components/UserRow.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class UserRow extends Component {
-    
+
     render() {
+        // destructure the user object props
+        const {
+            name,
+            location,
+            email,
+            dob: { date, age },
+            registered,
+            phone,
+            cell,
+            picture: { thumbnail }
+        } = this.props;
+
         return (
             <tr>
                 <td></td>
