664cff3e0ed6c2241fed38f69c4be83dbf4841b8
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-08 02:57:24 -0500

    add seed to api call to return the same users

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const API = {
     getRandomUsers: (numUsers) => {
         let url;
-        if (numUsers) url = `https://randomuser.me/api/?results=${numUsers}&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
-        else url = `https://randomuser.me/api/?results=100&inc=name,dob,picture,email,phone,cell,nat&noinfo`;
+        let seed = 'seedmeplease';
+        if (numUsers) url = `https://randomuser.me/api/?results=${numUsers}&seed=${seed}&inc=name,dob,picture,email,phone,cell,location&noinfo`;
+        else url = `https://randomuser.me/api/?results=100&seed=${seed}&inc=name,dob,picture,email,phone,cell,location&noinfo`;
 
         return axios.get(url);
     }
