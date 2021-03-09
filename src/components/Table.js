b2e076bdb1e55c64e262a063acabe1717fe57fd7
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-09 13:17:17 -0500

    add a sorting function

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,29 @@ import UserRow from './UserRow';
 class Table extends Component {
     state = {
         employees: [],
-        sorted: []
+        sortedEmployees: [],
+        sortCriteria: '',
+        sortOrder: '',
+        isSorted: false,
+    }
+
+    sortEmployees() {
+        let list = this.state.sortedEmployees;
+        if (!this.isSorted) return this.setState({ sortedEmployees: this.state.employees });
+
+        switch (this.state.sortCriteria) {
+            case 'first name':
+                if (this.state.sortOrder === 'ascending') list = list.sort((a, b) => a.firstName > b.firstName ? -1 : 1);
+                else list = list.sort((a, b) => a.firstName < b.firstName ? -1 : 1);
+                break;
+            case 'last name':
+                if (this.state.sortOrder === 'ascending') list = list.sort((a, b) => a.lastName > b.lastName ? -1 : 1);
+                else list = list.sort((a, b) => a.lastName < b.lastName ? -1 : 1);
+                break;
+            default:
+                break;
+        }
+
     }
 
     // make api call when the table mounts
@@ -14,6 +36,7 @@ class Table extends Component {
         API.getRandomUsers(5)
             .then(({ data }) => {
                 const employees = data.results.map(employee => {
+                    // returns an object with no nested objects
                     return {
                         firstName: employee.name.first,
                         lastName: employee.name.last,
@@ -23,9 +46,10 @@ class Table extends Component {
                         thumbnail: employee.picture.thumbnail
                     }
                 });
-
+                // sets the employees state to the array with the cleaned data
                 this.setState({
-                    employees: [...employees]
+                    employees: [...employees],
+                    sortedEmployees: [...employees]
                 });
             }).catch(err => console.error(err));
     }
@@ -44,7 +68,7 @@ class Table extends Component {
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.employees.map(employee => {
+                        {this.state.sortedEmployees.map(employee => {
                             return (
                                 <UserRow key={employee.email} employee={employee} />
                             );
