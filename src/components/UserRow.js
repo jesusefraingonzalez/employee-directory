4e59f6628d3545cf0709b75ca5cd78cfa0008727
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-09 01:46:15 -0500

    refactor for cleaner data

--- src/components/UserRow.js
+++ src/components/UserRow.js
@@ -11,30 +11,14 @@ class UserRow extends Component {
     }
 
     componentDidMount() {
-        // destructure the user object props
-        const {
-            name,
-            dob,
-            email,
-            phone,
-            cell,
-            picture: { thumbnail }
-        } = this.props.employee;
-
-        this.setState({
-            name: name,
-            dob: dob,
-            email: email,
-            phone: phone,
-            cell: cell,
-            picture: thumbnail,
-        });
+        this.setState(this.props.employee);
     }
+    
     render() {
         return (
             <tr>
-                <td><img src={this.state.picture}/></td>
-                <td>{`${this.state.name.first} ${this.state.name.last}`}</td>
+                <td><img src={this.state.thumbnail} /></td>
+                <td>{`${this.state.firstName} ${this.state.lastName}`}</td>
                 <td>{this.state.phone}</td>
                 <td>{this.state.cell}</td>
                 <td>{this.state.email}</td>
