a8ceddeeca056e27f52782696a96fb69522f2374
Efrain Gonzalez <jesusgonzalez0797@gmail.com>
2021-03-09 00:37:28 -0500

    add searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Searchbar from './components/Searchbar';
 import Table from './components/Table';
 
 function App() {
   return (
     <div>
+      <Searchbar />
       <Table />
     </div>
   );
